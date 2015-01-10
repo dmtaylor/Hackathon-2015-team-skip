0c3d277c93ce3b72a61862e63da6a3b9c80679ba
Justin Lardinois <me@justinlardinois.com>
2015-01-10 12:04:56 -0800

    fixed white outline?

--- src/graphix.c
+++ src/graphix.c
@@ -36,7 +36,7 @@ void canvas_update_proc(Layer *this_layer, GContext *ctx)
 	graphics_fill_rect(ctx,GRect(x_center - (HEALTH_BAR_LENGTH / 2)
 		- HEALTH_BAR_OUTLINE_WHITE,HEALTH_BAR_MARGIN_TOP
 		- HEALTH_BAR_OUTLINE_WHITE,HEALTH_BAR_LENGTH
-		+ 2*HEALTH_BAR_OUTLINE_WHITE,HEALTH_BAR_HEIGHT
+		+ HEALTH_BAR_OUTLINE_WHITE,HEALTH_BAR_HEIGHT
 		+ 2*HEALTH_BAR_OUTLINE_WHITE),0,GCornerNone);
 
 	// health fill
