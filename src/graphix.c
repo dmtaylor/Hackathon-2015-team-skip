3eb22f9e19d893d3de2c49231e190537c283cf93
Justin Lardinois <me@justinlardinois.com>
2015-01-10 08:44:36 -0800

    moved stroke call; fixed arguments

--- src/graphix.c
+++ src/graphix.c
@@ -7,7 +7,7 @@ static Layer *s_canvas_layer;
 
 void canvas_update_proc(Layer *this_layer, GContext *ctx)
 {
-
+	graphics_context_set_stroke_color(ctx,GColorBlack);
 }
 
 void __load(Window* window)
@@ -28,7 +28,6 @@ void __unload(Window* window)
 
 void graphix(uint32_t max_health , uint32_t curr_health , uint32_t prev_dmg)
 {
-	graphics_context_set_stroke_color(GColorBlack);
 	s_main_window = window_create();
 	window_set_window_handlers(s_main_window,(WindowHandlers){
 		.load = __load;
