321dbdff5cc0fa4633e9b154dd39f15a80975f25
Justin Lardinois <me@justinlardinois.com>
2015-01-10 15:44:45 -0800

    added timer for testing graphix updates

--- src/shadowboxer.c
+++ src/shadowboxer.c
@@ -2,9 +2,17 @@
 
 #include "graphix.h"
 
+uint32_t test_max_health = 200;
+uint32_t test_curr_health = test_max_health;
+
+void timer_test_handler()
+{
+	graphix(test_max_health,test_curr_health,0);
+	app_timer_register(1,timer_test_handler,NULL);
+}
+
 void init(){
-  graphix(200,170,0);
-  
+  timer_test_handler();
 }
 
 void deinit(){
