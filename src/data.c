5245fa7c07d97ccce666827789eeee772ed95026
David M. Taylor <dmtaylor2011@gmail.com>
2015-01-10 12:28:15 -0800

    Added player updating functions.
    Added more functionality from the header.

--- src/data.c
+++ src/data.c
@@ -106,3 +106,80 @@ monster_info* gen_monster(uint16_t monster_level){
 	
 	return monster;
 }
+
+char monster_is_dead(monster_info* monster){
+	if(monster == NULL){
+		fprintf(stderr, "Error: Null monster pointer\n");
+		return 255;
+	}
+	if(monster->curr_health == 0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+void update_monster_health(monster_info* monster, uint32_t damage){
+	if(monster == NULL){
+		fprintf(stderr, "Error: Null monster to update health");
+		return;
+	}
+	
+	if(damage>=monster->curr_health){
+		monster->curr_health =0;
+	}
+	else {
+		monster->curr_health = monster->curr_health - damage;
+	}
+	return;
+}
+
+char get_monster_type(monster_info* monster){
+	if(monster == NULL){
+		fprintf(stderr, "Error: Null monster to get type\n");
+		return 0;
+	}
+	return monster->type;
+}
+
+void update_player_info(player_info* player, monster_info* monster){
+	if(player== NULL){
+		fprintf(stderr, "Error: player info null, cannot be updated\n");
+		return;
+	}
+	if(monster == NULL){
+		fprintf(stderr, "Error: monster info null, player cannot be updated\n");
+		return;
+	}
+	int new_xp = player->curr_xp + 2*monster->level;
+	
+	if(new_xp >= player->next_xp){
+		player->curr_xp = 0;
+		player->curr_level++;
+		player->next_xp *= 2;
+	}
+	else{
+		player->curr_xp = new_xp;
+	}
+	
+	return;
+}
+
+uint16_t get_player_level(player_info* player){
+	if(player == NULL){
+		fprintf(stderr, "Error: Null player to get level");
+		return 0;
+	}
+	return player->level;
+}
+
+double get_damage_mod(player_info* player){
+	if(player == NULL){
+		fprintf(stderr, "Error: Null player to get mod");
+		return 0;
+	}
+	return player->damage_mod;
+}
+
+
