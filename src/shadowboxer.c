4872e4dbe7b2cb165b3a1a8d979a7b3ff500c912
David M. Taylor <dmtaylor2011@gmail.com>
2015-01-10 17:58:33 -0800

    Bugfixing attempt at persistent data storage

--- src/shadowboxer.c
+++ src/shadowboxer.c
@@ -12,22 +12,27 @@ uint16_t game_level;
 
 void init(){
 	
-	// Loads stored game data
+	/*// Loads stored game data
 	if(persist_exists(PLAYER_STORE)){
 		persist_read_data(PLAYER_STORE, player, sizeof(player_info));
 	}
 	else{
 		player = build_player();
-	}
+	}*/
+	
+	// testing reading and writing
+	player = build_player();
 	
 	game_level = player->curr_level;
 	
-	if(persist_exists(MONSTER_STORE)){
+	/*if(persist_exists(MONSTER_STORE)){
 		persist_read_data(MONSTER_STORE, monster, sizeof(monster_info));
 	}
 	else{
 		monster = gen_monster(game_level);
-	}
+	}*/
+	
+	monster = gen_monster(game_level);
 	
 	graphix(200,200,50);
 }
