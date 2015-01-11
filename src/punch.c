949ffbdbd225624d4aa822e98e7e5066af3d9a6e
Sebastien Young <sebastien@young.cc>
2015-01-11 03:28:13 -0800

    changed the punch settings

--- src/punch.c
+++ src/punch.c
@@ -132,9 +132,9 @@ int punch_callback(void *data) {
 
 static void reg_callback(void *data){
   int p_type = punch_callback(data);
-  if(p_type == 1) vibes_short_pulse();
-  else if (p_type == 2) vibes_double_pulse();
-  else if (p_type == 3) vibes_long_pulse();
+  if(p_type == 1) vibes_double_pulse();
+  else if (p_type == 2) vibes_long_pulse();
+  else if (p_type == 3) vibes_short_pulse();
   
    //APP_LOG(APP_LOG_LEVEL_INFO,"T: %d", p_type);
 
