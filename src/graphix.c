1631f52a3915b23698bc90946856d214d3502f76
Justin Lardinois <me@justinlardinois.com>
2015-01-10 22:13:28 -0800

    first finger line in HOOK

--- src/graphix.c
+++ src/graphix.c
@@ -67,6 +67,9 @@ void draw_punch(Layer *this_layer , GContext* ctx , enum punch_type punch_type)
 				20,30),0,GCornerNone);
 			graphics_fill_rect(ctx,GRect(bounds.size.w - 14,y_center - 15,
 				5,10),0,GCornerNone);
+
+			graphics_draw_line(ctx,GPoint(bounds.size.w - 28,y_center - 15),
+				GPoint(bounds.size.w - 15,y_center - 15),0,GCornerNone);
 			break;
 		case UPPERCUT:
 			break;
