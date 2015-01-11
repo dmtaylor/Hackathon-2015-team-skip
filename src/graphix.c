c2cefa2fa72aaddf1d0ef482031e1c11a66b4114
Justin Lardinois <me@justinlardinois.com>
2015-01-10 21:11:23 -0800

    adjusting the line

--- src/graphix.c
+++ src/graphix.c
@@ -50,7 +50,7 @@ void draw_punch(Layer *this_layer , GContext* ctx , enum punch_type punch_type)
 			graphics_fill_rect(ctx,GRect(bounds.size.w - 40,y_center - 10,
 				30,20),0,GCornerNone);
 			graphics_context_set_stroke_color(ctx,GColorWhite);
-			graphics_draw_line(ctx,GPoint(bounds.size.w - 32,y_center - 5),
+			graphics_draw_line(ctx,GPoint(bounds.size.w - 32,y_center),
 				GPoint(bounds.size.w - 32,y_center + 10));
 			break;
 		case HOOK:
