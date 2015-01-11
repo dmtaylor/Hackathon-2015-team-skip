73a2b3c112ddf12d906d4d9043d8453b3e621c2c
Justin Lardinois <me@justinlardinois.com>
2015-01-10 21:24:26 -0800

    stab in the dark with the thumb

--- src/graphix.c
+++ src/graphix.c
@@ -49,6 +49,8 @@ void draw_punch(Layer *this_layer , GContext* ctx , enum punch_type punch_type)
 			graphics_context_set_fill_color(ctx,GColorBlack);
 			graphics_fill_rect(ctx,GRect(bounds.size.w - 40,y_center - 10,
 				30,20),0,GCornerNone);
+			graphics_fill_rect(ctx,GRect(bounds.size.w - 18,y_center + 11,
+				8,5),0,GCornerNone);
 			graphics_context_set_stroke_color(ctx,GColorWhite);
 			graphics_draw_line(ctx,GPoint(bounds.size.w - 33,y_center-3),
 				GPoint(bounds.size.w - 33,y_center + 10));
