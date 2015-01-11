6544ad7015c096faed966d42627f5b9997162569
Justin Lardinois <me@justinlardinois.com>
2015-01-10 19:27:51 -0800

    bottom padding for monster name

--- src/graphix.c
+++ src/graphix.c
@@ -21,6 +21,7 @@
 #define NAME_BUFFER_SIZE         128
 #define NAME_WIDTH               bounds.size.w
 #define NAME_HEIGHT               20
+#define NAME_MARGIN_BOTTOM        10
 
 static Window *s_main_window;
 static Layer *s_canvas_layer;
@@ -94,8 +95,8 @@ void canvas_update_proc(Layer *this_layer, GContext *ctx)
 	snprintf(name_buffer,NAME_BUFFER_SIZE,"%s %s",_adj,_name);
 	graphics_draw_text(ctx,name_buffer,
 		fonts_get_system_font(FONT_KEY_GOTHIC_24),GRect(x_center
-		- (NAME_WIDTH / 2),bounds.size.h - NAME_HEIGHT,NAME_WIDTH,
-		NAME_HEIGHT),GTextOverflowModeTrailingEllipsis,
+		- (NAME_WIDTH / 2),bounds.size.h - NAME_HEIGHT - NAME_MARGIN_BOTTOM,
+		NAME_WIDTH,NAME_HEIGHT),GTextOverflowModeTrailingEllipsis,
 		GTextAlignmentCenter,NULL);
 }
 
