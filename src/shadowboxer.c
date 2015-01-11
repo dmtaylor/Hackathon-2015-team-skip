2bde7c0a641314bf24a6bc944fd843dec3cf9722
Justin Lardinois <me@justinlardinois.com>
2015-01-10 16:05:19 -0800

    switched to ticktimer for more testing

--- src/shadowboxer.c
+++ src/shadowboxer.c
@@ -8,12 +8,11 @@ uint32_t test_curr_health = 200;
 void timer_test_handler()
 {
 	graphix(test_max_health,test_curr_health,0);
-	app_timer_register(500,timer_test_handler,NULL);
 	test_curr_health--;
 }
 
 void init(){
-  timer_test_handler();
+	tick_timer_service_subscribe(SECOND_UNIT,timer_test_handler);
 }
 
 void deinit(){
